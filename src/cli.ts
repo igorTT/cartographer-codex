3b32069e7f551d698764154091ecf1f63ee33048
Igor <hirzhek@gmail.com>
2026-08-20 21:41:14 +0200

    v0.3.2: Prepare release

--- src/cli.ts
+++ src/cli.ts
@@ -7,7 +7,7 @@ const program = new Command();
 program
   .name("cartodex")
   .description("Cartodex repository initializer for Codex.")
-  .version("0.3.1");
+  .version("0.3.2");
 
 registerInitCommand(program);
 
